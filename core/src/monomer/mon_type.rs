60babebd93468858576d025abf6cca859dc5fa2f
Keith Oshima <koshima789@gmail.com>
2025-01-23 00:04:32 -0500

    Renamed Monomer monomer_type and monomer_type_dec to hor and hor_desc.

--- core/src/monomer/mon_type.rs
+++ core/src/monomer/mon_type.rs
@@ -3,7 +3,7 @@ use std::{fmt::Display, str::FromStr};
 use eyre::bail;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum MonomerType {
+pub enum MonomerHOR {
     H1,
     H2,
     H3,
@@ -15,47 +15,47 @@ pub enum MonomerType {
     H9,
 }
 
-impl FromStr for MonomerType {
+impl FromStr for MonomerHOR {
     type Err = eyre::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
-            "H1" => MonomerType::H1,
-            "H2" => MonomerType::H2,
-            "H3" => MonomerType::H3,
-            "H4" => MonomerType::H4,
-            "H5" => MonomerType::H5,
-            "H6" => MonomerType::H6,
-            "H7" => MonomerType::H7,
-            "H8" => MonomerType::H8,
-            "H9" => MonomerType::H9,
+            "H1" => MonomerHOR::H1,
+            "H2" => MonomerHOR::H2,
+            "H3" => MonomerHOR::H3,
+            "H4" => MonomerHOR::H4,
+            "H5" => MonomerHOR::H5,
+            "H6" => MonomerHOR::H6,
+            "H7" => MonomerHOR::H7,
+            "H8" => MonomerHOR::H8,
+            "H9" => MonomerHOR::H9,
             _ => bail!("Unknown monomer type, {s}."),
         })
     }
 }
 
-impl Display for MonomerType {
+impl Display for MonomerHOR {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{}",
             match self {
-                MonomerType::H1 => "H1",
-                MonomerType::H2 => "H2",
-                MonomerType::H3 => "H3",
-                MonomerType::H4 => "H4",
-                MonomerType::H5 => "H5",
-                MonomerType::H6 => "H6",
-                MonomerType::H7 => "H7",
-                MonomerType::H8 => "H8",
-                MonomerType::H9 => "H9",
+                MonomerHOR::H1 => "H1",
+                MonomerHOR::H2 => "H2",
+                MonomerHOR::H3 => "H3",
+                MonomerHOR::H4 => "H4",
+                MonomerHOR::H5 => "H5",
+                MonomerHOR::H6 => "H6",
+                MonomerHOR::H7 => "H7",
+                MonomerHOR::H8 => "H8",
+                MonomerHOR::H9 => "H9",
             }
         )
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum AncestralMonomerTypes {
+pub enum AncestralMonomer {
     W1,
     Ca,
     La,
@@ -86,39 +86,39 @@ pub enum AncestralMonomerTypes {
     W4,
 }
 
-impl FromStr for AncestralMonomerTypes {
+impl FromStr for AncestralMonomer {
     type Err = eyre::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
-            "W1" => AncestralMonomerTypes::W1,
-            "Ca" => AncestralMonomerTypes::Ca,
-            "La" => AncestralMonomerTypes::La,
-            "Ba" => AncestralMonomerTypes::Ba,
-            "Ja" => AncestralMonomerTypes::Ja,
-            "Na" => AncestralMonomerTypes::Na,
-            "Fa" => AncestralMonomerTypes::Fa,
-            "Oa" => AncestralMonomerTypes::Oa,
-            "J1" => AncestralMonomerTypes::J1,
-            "R1" => AncestralMonomerTypes::R1,
-            "W3" => AncestralMonomerTypes::W3,
-            "Aa" => AncestralMonomerTypes::Aa,
-            "M1" => AncestralMonomerTypes::M1,
-            "R2" => AncestralMonomerTypes::R2,
-            "Ea" => AncestralMonomerTypes::Ea,
-            "Ia" => AncestralMonomerTypes::Ia,
-            "W5" => AncestralMonomerTypes::W5,
-            "Qa" => AncestralMonomerTypes::Qa,
-            "Ga" => AncestralMonomerTypes::Ga,
-            "Ta" => AncestralMonomerTypes::Ta,
-            "D2" => AncestralMonomerTypes::D2,
-            "W2" => AncestralMonomerTypes::W2,
-            "D1" => AncestralMonomerTypes::D1,
-            "Ka" => AncestralMonomerTypes::Ka,
-            "Ha" => AncestralMonomerTypes::Ha,
-            "Pa" => AncestralMonomerTypes::Pa,
-            "J2" => AncestralMonomerTypes::J2,
-            "W4" => AncestralMonomerTypes::W4,
+            "W1" => AncestralMonomer::W1,
+            "Ca" => AncestralMonomer::Ca,
+            "La" => AncestralMonomer::La,
+            "Ba" => AncestralMonomer::Ba,
+            "Ja" => AncestralMonomer::Ja,
+            "Na" => AncestralMonomer::Na,
+            "Fa" => AncestralMonomer::Fa,
+            "Oa" => AncestralMonomer::Oa,
+            "J1" => AncestralMonomer::J1,
+            "R1" => AncestralMonomer::R1,
+            "W3" => AncestralMonomer::W3,
+            "Aa" => AncestralMonomer::Aa,
+            "M1" => AncestralMonomer::M1,
+            "R2" => AncestralMonomer::R2,
+            "Ea" => AncestralMonomer::Ea,
+            "Ia" => AncestralMonomer::Ia,
+            "W5" => AncestralMonomer::W5,
+            "Qa" => AncestralMonomer::Qa,
+            "Ga" => AncestralMonomer::Ga,
+            "Ta" => AncestralMonomer::Ta,
+            "D2" => AncestralMonomer::D2,
+            "W2" => AncestralMonomer::W2,
+            "D1" => AncestralMonomer::D1,
+            "Ka" => AncestralMonomer::Ka,
+            "Ha" => AncestralMonomer::Ha,
+            "Pa" => AncestralMonomer::Pa,
+            "J2" => AncestralMonomer::J2,
+            "W4" => AncestralMonomer::W4,
             _ => bail!("Unknown monomer type, {s}."),
         })
     }
